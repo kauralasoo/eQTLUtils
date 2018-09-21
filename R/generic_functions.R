0348e59be60bbbb4d1e140f1de1f1facabc6499b
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-09-21 18:24:18 +0300

    First version of the eQTLUtils package

---
+++ R/generic_functions.R
@@ -0,0 +1,30 @@
+#' A general function to quickly import tabix indexed tab-separated files into data_frame
+#'
+#' @param tabix_file Path to tabix-indexed text file
+#' @param param A instance of GRanges, RangedData, or RangesList
+#' provide the sequence names and regions to be parsed. Passed onto Rsamtools::scanTabix()
+#' @param ... Additional parameters to be passed on to readr::read_delim()
+#'
+#' @return List of data_frames, one for each entry in the param GRanges object.
+#' @export
+scanTabixDataFrame <- function(tabix_file, param, ...){
+  tabix_list = Rsamtools::scanTabix(tabix_file, param = param)
+  df_list = lapply(tabix_list, function(x,...){
+    if(length(x) > 0){
+      if(length(x) == 1){
+        #Hack to make sure that it also works for data frames with only one row
+        #Adds an empty row and then removes it
+        result = paste(paste(x, collapse = "\n"),"\n",sep = "")
+        result = readr::read_delim(result, delim = "\t", ...)[1,]
+      }else{
+        result = paste(x, collapse = "\n")
+        result = readr::read_delim(result, delim = "\t", ...)
+      }
+    } else{
+      #Return NULL if the nothing is returned from tabix file
+      result = NULL
+    }
+    return(result)
+  }, ...)
+  return(df_list)
+}
---
+++ R/qtl_QTLTools.R
@@ -0,0 +1,138 @@
+#' Save a list of matrices into a suitable format for FastQTL
+#'
+#' Works with expression and covariates matrices.
+#'
+#' @param data_list list of matrices
+#' @param output_dir relative path to the output dir
+#' @param file_suffix suffix added to each file after their name in the list.
+#' @return None
+#' @author Kaur Alasoo
+#' @export
+saveQTLToolsMatrices <- function(data_list, output_dir, file_suffix = "bed", col_names = TRUE){
+  #Save data for FastQTL to disk
+
+  #Save each matrix as a separate  txt file
+  for (sn in names(data_list)){
+    file_path = file.path(output_dir, paste(sn, file_suffix, sep = "."))
+    print(file_path)
+    write.table(data_list[[sn]], file_path, sep = "\t", quote = FALSE, row.names = FALSE, col.names = col_names)
+  }
+}
+
+#' Import QTLtools output table from permutation run into R.
+#'
+#'
+#' @param file_path Path to the QTLtools output file.
+#' @return data_frame containing gene_ids, snp ids and p-values.
+#' @author Kaur Alasoo
+#' @export
+importQTLtoolsTable <- function(file_path){
+  col_names = c("group_id", "pheno_chr", "pheno_start", "pheno_end", "strand", "phenotype_id", "group_size", "n_cis_snps",
+                "distance", "snp_id", "snp_chr", "snp_start", "snp_end", "df", "dummy", "beta1",
+                "beta2", "p_nominal","slope","p_perm","p_beta")
+  col_types = "cciicciiicciiiddddddd"
+  table = readr::read_delim(file_path, col_names = col_names, delim = " ", col_types = col_types) %>%
+    dplyr::filter(!is.na(p_beta)) %>%
+    dplyr::mutate(p_bonferroni = p_nominal*group_size*n_cis_snps) %>%
+    dplyr::mutate(p_bonferroni = pmin(p_bonferroni,1)) %>%
+    dplyr::mutate(p_fdr = p.adjust(p_beta, method = "fdr")) %>%
+    dplyr::mutate(qvalue = qvalue::qvalue(p_beta)$qvalues) %>%
+    dplyr::arrange(p_fdr)
+  return(table)
+}
+
+#' Fetch particular genes from tabix indexed QTLtools output file.
+#'
+#' @param gene_ranges GRanges object with coordinates of the cis regions around genes.
+#' @param tabix_file Tabix-indexed fastqtl output file.
+#'
+#' @return List of data frames containing QTLtools results for each gene.
+#' @export
+qtltoolsTabixFetchPhenotypes <- function(phenotype_ranges, tabix_file){
+
+  #Assertions about input
+  assertthat::assert_that(class(phenotype_ranges) == "GRanges")
+  assertthat::assert_that(assertthat::has_name(GenomicRanges::elementMetadata(phenotype_ranges), "phenotype_id"))
+
+  #Set column names for rasqual
+  fastqtl_columns = c("phenotype_id","pheno_chr","pheno_start", "pheno_end",
+                      "strand","n_snps", "distance", "snp_id", "snp_chr",
+                      "snp_start", "snp_end", "p_nominal","beta", "is_lead")
+  fastqtl_coltypes = "cciiciicciiddi"
+
+  result = list()
+  for (i in seq_along(phenotype_ranges)){
+    selected_phenotype_id = phenotype_ranges[i]$phenotype_id
+    print(i)
+    tabix_table = scanTabixDataFrame(tabix_file, phenotype_ranges[i],
+                                     col_names = fastqtl_columns, col_types = fastqtl_coltypes)[[1]] %>%
+      dplyr::filter(phenotype_id == selected_phenotype_id)
+
+    #Add additional columns
+    result[[selected_phenotype_id]] = tabix_table
+  }
+  return(result)
+}
+
+
+mbvFindBestMatch <- function(mbv_df){
+  res = dplyr::transmute(mbv_df, mbv_genotype_id = SampleID,
+                         het_consistent_frac = n_het_consistent/n_het_covered,
+                         hom_consistent_frac = n_hom_consistent/n_hom_covered)
+
+  #Identify best het
+  best_het = dplyr::arrange(res, -het_consistent_frac) %>% dplyr::filter(dplyr::row_number() == 1)
+  other_het = dplyr::arrange(res, -het_consistent_frac) %>% dplyr::filter(dplyr::row_number() > 1)
+  best_row = dplyr::mutate(best_het, het_min_dist = min(best_het$het_consistent_frac - other_het$het_consistent_frac),
+                           hom_min_dist = min(best_het$hom_consistent_frac - other_het$hom_consistent_frac))
+
+  #Compare against best hom
+  best_hom = dplyr::arrange(res, -hom_consistent_frac) %>% dplyr::filter(dplyr::row_number() == 1)
+  if(best_row$mbv_genotype_id != best_hom$mbv_genotype_id){
+    best_row = dplyr::mutate(best_row, het_consistent_frac = as.numeric(NA), hom_consistent_frac = as.numeric(NA),
+                             het_min_dist = as.numeric(NA), hom_min_dist = as.numeric(NA))
+  }
+  return(best_row)
+}
+
+
+#' Convert SummarizedExperiment object into a bed file suitable for QTLTools
+#'
+#' @param se SummarizedExperiment object
+#' @param assay_name Assay of the SummarizedExperiment object used for QTL mapping
+#'
+#' @return Assay converted into bed format suitable for QTLTools
+#' @export
+convertSEtoQTLtools <- function(se, assay_name = "cqn"){
+
+  #Extract rowData from the SE
+  phenotype_data = rowData(se) %>%
+    as.data.frame() %>%
+    dplyr::as_tibble()
+
+  #Make sure that all required columns are present
+  assertthat::assert_that(assertthat::has_name(phenotype_data, "chromosome"))
+  assertthat::assert_that(assertthat::has_name(phenotype_data, "phenotype_pos"))
+  assertthat::assert_that(assertthat::has_name(phenotype_data, "phenotype_id"))
+  assertthat::assert_that(assertthat::has_name(phenotype_data, "group_id"))
+  assertthat::assert_that(!is.null(se$genotype_id))
+
+  #Make genePos table for QTLTools
+  pheno_data = dplyr::arrange(phenotype_data, chromosome, phenotype_pos) %>%
+    dplyr::transmute(chromosome, left = phenotype_pos, right = phenotype_pos, phenotype_id, group_id, strand) %>%
+    dplyr::rename_("#chr" = "chromosome") %>%
+    dplyr::mutate(strand = ifelse(strand == 1, "+", "-"))
+
+  #Exptract phenotype and rename columns according to genotype id
+  assay = assays(se)[[assay_name]]
+  colnames(assay) = se$genotype_id
+  assay = round(assay, 3) #Round to three digits
+
+  #Make QTLtools phenotype table
+  res = dplyr::mutate(as.data.frame(assay), phenotype_id = rownames(assay)) %>%
+    dplyr::select(phenotype_id, everything()) %>%
+    dplyr::left_join(pheno_data, ., by = "phenotype_id") %>%
+    dplyr::arrange()
+
+  return(res)
+}
