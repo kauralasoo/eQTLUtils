8b2e98d757711559ca80c189e79691ff4fba3e09
kerimoff <kerimov.nurlan@gmail.com>
2019-04-29 10:20:57 +0300

    changes to RNA_QC utils

--- R/rna_qc_utils.R
+++ R/rna_qc_utils.R
@@ -52,29 +52,38 @@ plot_mbv_results <- function(mbv_files_path, output_path){
 #' @author Nurlan Kerimov
 #' @export
 plotSexQC <- function(study_data, export_output = FALSE, html_output=FALSE, output_dir="./"){
-  joined <- calculateSexQCDataFrame(study_data)
+  sex_qc_df <- calculateSexQCDataFrame(study_data)
   
-  if (all(is.na(joined$sex))) { joined$sex <- "NA"}
+  if (all(is.na(sex_qc_df$sex))) { sex_qc_df$sex <- "NA"}
   
-  study_name <- study_data$study %>% unique()
-  # generate the plot
-  Sex_QC_plot <- ggplot2::ggplot(joined, 
-    ggplot2::aes(x=(ENSG00000229807+1) %>% log2(), y=(Y_chrom_mean+1) %>% log2(), label = sample_id)) + 
-    ggplot2::geom_point(ggplot2::aes(col=sex)) +
-    ggplot2::labs(x="Expression XIST", y="Expression genes on Y", title = paste0(study_name, " DS - TPM normalized, log2 | Sample Size: ", nrow(joined))) +
-    ggplot2::theme(plot.title = element_text(size = 12, face="italic"))
-
-
   if (export_output){
-    output_dir <- paste0(output_dir, "/Sex_QC_plot/")
+    study_name <- study_data$study %>% unique()
+    # generate the plot
+    Sex_QC_plot <- ggplot2::ggplot(sex_qc_df, 
+      ggplot2::aes(x=(ENSG00000229807+1) %>% log2(), y=(Y_chrom_mean+1) %>% log2(), label = sample_id)) + 
+      ggplot2::geom_point(ggplot2::aes(col=sex)) +
+      ggplot2::labs(x="Expression XIST", y="Expression genes on Y", title = paste0(study_name, " DS - TPM normalized, log2 | Sample Size: ", nrow(sex_qc_df))) +
+      ggplot2::theme(plot.title = element_text(size = 12, face="italic"))
+
+  
+    output_dir <- paste0(output_dir, "/Sex_spec_gene_exp_plot/")
     if (!dir.exists(output_dir)) { 
       dir.create(paste0(output_dir, "/jpeg/"), recursive = TRUE)
     }
-    ggplot2::ggsave(filename = paste0(study_name, "_Sex_QC_Plot.jpeg"), 
+    if (!dir.exists(paste0(output_dir, "/pdf/"))) { 
+      dir.create(paste0(output_dir, "/pdf/"), recursive = TRUE)
+    }
+    ggplot2::ggsave(filename = paste0(study_name, "_Sex_spec_gene_exp.jpeg"), 
                     width = 6, height = 4, dpi = 300,
                     plot = Sex_QC_plot, 
                     path = paste0(output_dir, "/jpeg"), 
                     device = "jpeg")
+    
+    ggplot2::ggsave(filename = paste0(study_name, "_Sex_spec_gene_exp.pdf"), 
+                    width = 7, height = 3, dpi = 300,
+                    plot = Sex_QC_plot, 
+                    path = paste0(output_dir, "/pdf"), 
+                    device = "pdf")
     
     if (html_output) {
       if (!dir.exists(paste0(output_dir, "/plotly/dependencies/"))) { 
@@ -88,7 +97,7 @@ plotSexQC <- function(study_data, export_output = FALSE, html_output=FALSE, outp
     }
   }
   
-  return(Sex_QC_plot)
+  return(sex_qc_df)
 }
 
 #' Generate Sex dependent QC Plot.
@@ -109,7 +118,6 @@ plotSexQCFromMatrix <- function(sex_qc_matrix, study_name, export_output = FALSE
     ggplot2::labs(x="Expression XIST", y="Expression genes on Y", 
         title = paste0(study_name, " DS - TPM normalized, log2 | Sample Size: ", nrow(sex_qc_matrix))) +
     ggplot2::theme(plot.title = element_text(size = 12, face="italic"))
-  
   
   if (export_output){
     output_dir <- paste0(output_dir, "/Sex_QC_plot/")
@@ -207,11 +215,21 @@ plotMDSAnalysis <- function(study_data_se, condition = "all", export_output = FA
     if (!dir.exists(output_dir)) { 
       dir.create(paste0(output_dir, "/jpeg/"), recursive = TRUE)
     }
+    if (!dir.exists(paste0(output_dir, "/pdf/"))) { 
+      dir.create(paste0(output_dir, "/pdf/"), recursive = TRUE)
+    }
+    
     ggplot2::ggsave(filename = paste0(study_name, "_MDS.jpeg"), 
                     width = 7, height = 3, dpi = 300,
                     plot = mds_plot, 
                     path = paste0(output_dir, "/jpeg"), 
                     device = "jpeg")
+    
+    ggplot2::ggsave(filename = paste0(study_name, "_MDS.pdf"), 
+                    width = 7, height = 3, dpi = 300,
+                    plot = mds_plot, 
+                    path = paste0(output_dir, "/pdf"), 
+                    device = "pdf")
     
     if (html_output) {
       if (!dir.exists(paste0(output_dir, "/plotly/dependencies/"))) { 
@@ -225,7 +243,7 @@ plotMDSAnalysis <- function(study_data_se, condition = "all", export_output = FA
     }
   }
   
-  return(mds_plot)
+  return(mds_matrix)
 }
 
 #' Generate MDS QC Plot from PCA Matrix
@@ -235,7 +253,7 @@ plotMDSAnalysis <- function(study_data_se, condition = "all", export_output = FA
 #' @return PCA plot of study 
 #' @author Nurlan Kerimov
 #' @export
-plotMSDFromMatrix <- function(mds_matrix, export_output = FALSE, html_output=FALSE, output_dir="./"){
+plotMDSFromMatrix <- function(mds_matrix, export_output = FALSE, html_output=FALSE, output_dir="./"){
   study_name <- unique(mds_matrix$study)[1]
   
   mds_plot = ggplot2::ggplot(mds_matrix, ggplot2::aes(x = V1, y = V2, color = cell_type, shape = study, label = sample_id)) + 
@@ -283,8 +301,17 @@ calculateMDSMatrix <- function(study_data_se, condition = "all"){
     study_data_se = study_data_se[,study_data_se$condition == condition]
   }
   
-  # choose only protein coding genes and TPM normalise
-  processed_se = eQTLUtils::filterSummarizedExperiment(study_data_se, valid_gene_types = "protein_coding") %>% eQTLUtils::normaliseSE_tpm()
+  valid_gene_types = c("lincRNA","protein_coding","IG_C_gene","IG_D_gene","IG_J_gene",
+                       "IG_V_gene", "TR_C_gene","TR_D_gene","TR_J_gene", "TR_V_gene",
+                       "3prime_overlapping_ncrna","known_ncrna", "processed_transcript",
+                       "antisense","sense_intronic","sense_overlapping")
+  valid_chromosomes = c("1","10","11","12","13","14","15","16","17","18","19",
+                        "2","20","21","22","3","4","5","6","7","8","9")
+  
+  # choose only valid gene types, valid chromosomes and TPM normalise
+  processed_se = eQTLUtils::filterSummarizedExperiment(study_data_se,
+                                                       valid_chromosomes = valid_chromosomes,
+                                                       valid_gene_types = valid_gene_types) %>% eQTLUtils::normaliseSE_tpm()
   processed_se = processed_se[apply(SummarizedExperiment::assays(processed_se)$tpms, 1, median) > 1,]
   
   #Perform MDS
@@ -293,8 +320,7 @@ calculateMDSMatrix <- function(study_data_se, condition = "all"){
   fit <- MASS::isoMDS(1-dist, k=2)
   
   mds_matrix = SummarizedExperiment::as.data.frame(fit$points) %>%
-    as_tibble() %>%
-    dplyr::mutate(sample_id = rownames(fit$points)) %>%
+    as_tibble() %>% dplyr::mutate(sample_id = rownames(fit$points)) %>%
     dplyr::left_join(SummarizedExperiment::as.data.frame(SummarizedExperiment::colData(processed_se)), by = "sample_id")
   
   return(mds_matrix)
@@ -310,25 +336,37 @@ calculateMDSMatrix <- function(study_data_se, condition = "all"){
 #' @author Nurlan Kerimov
 #' @export
 plotPCAAnalysis <- function(study_data_se, condition = "all", export_output = FALSE, html_output=FALSE, output_dir="./"){
-  pca_matrix = calculatePCAMatrix(study_data_se, condition)
+  pca_res = calculatePCAMatrix(study_data_se, condition, return_pca_object = TRUE)
   study_name <- study_data_se$study %>% unique()
   
-  PCA.plot <- ggplot2::ggplot(pca_matrix, ggplot2::aes(x = PC1, y = PC2, color = cell_type, shape = study, label = sample_id)) + 
+  PCA.plot <- ggplot2::ggplot(pca_res$pca_matrix, ggplot2::aes(x = PC1, y = PC2, color = cell_type, shape = study, label = sample_id)) + 
     ggplot2::geom_point() + 
     ggplot2::scale_shape_manual(values=c(20,17,18,11,14,25,8,seq(0,7))) + 
-    ggplot2::labs(x="PC 1", y="PC 2", title = paste0(study_name, " PCA - TPM normalized, log2 | Sample Size: ", nrow(study_data_se %>% SummarizedExperiment::colData()))) +
+    ggplot2::labs(x=paste0("PC 1 - (", round(pca_res$var_exp[1]*100, digits = 1),"% var. explained)"), 
+                  y=paste0("PC 2 - (", round(pca_res$var_exp[2]*100, digits = 1),"% var. explained)"), 
+                  title = paste0(study_name, " PCA - TPM normalized, log2 | Sample Size: ", nrow(study_data_se %>% SummarizedExperiment::colData()))) +
     ggplot2::theme(plot.title = element_text(size = 12, face="italic"))
   
   if (export_output){
     output_dir <- paste0(output_dir, "/PCA_plot/")
-    if (!dir.exists(output_dir)) { 
+    if (!dir.exists(paste0(output_dir, "/jpeg/"))) { 
       dir.create(paste0(output_dir, "/jpeg/"), recursive = TRUE)
     }
+    if (!dir.exists(paste0(output_dir, "/pdf/"))) { 
+      dir.create(paste0(output_dir, "/pdf/"), recursive = TRUE)
+    }
+    
     ggplot2::ggsave(filename = paste0(study_name, "_PCA.jpeg"), 
                     width = 7, height = 3, dpi = 300,
                     plot = PCA.plot, 
                     path = paste0(output_dir, "/jpeg"), 
                     device = "jpeg")
+    
+    ggplot2::ggsave(filename = paste0(study_name, "_PCA.pdf"), 
+                    width = 7, height = 3, dpi = 300,
+                    plot = PCA.plot, 
+                    path = paste0(output_dir, "/pdf"), 
+                    device = "pdf")
     
     if (html_output) {
       if (!dir.exists(paste0(output_dir, "/plotly/dependencies/"))) { 
@@ -342,7 +380,7 @@ plotPCAAnalysis <- function(study_data_se, condition = "all", export_output = FA
     }
   }
   
-  return(PCA.plot)
+  return(pca_res)
 }
 
 
@@ -392,21 +430,35 @@ plotPCAFromMatrix <- function(pca_matrix, export_output = FALSE, html_output=FAL
 #'
 #' @param study_data_se SummarizedExperiment file to be analysed
 #' @param condition if needed PCA matrix of specific condition
+#' @param return_pca_object boolean value: return the whole pca object instead of matrix
 #' @return PCA Matrix of study 
 #' @author Nurlan Kerimov
 #' @export
-calculatePCAMatrix <- function(study_data_se, condition = "all"){
+calculatePCAMatrix <- function(study_data_se, condition = "all", return_pca_object = FALSE){
   if (condition!="all") {
     study_data_se = study_data_se[,study_data_se$condition == condition]
   }
   
-  # choose only protein coding genes and TPM normalise
-  processed_se = eQTLUtils::filterSummarizedExperiment(study_data_se, valid_gene_types = "protein_coding") %>% eQTLUtils::normaliseSE_tpm()
-  processed_se = processed_se[apply(SummarizedExperiment::assays(processed_se)$tpms, 1, median) > 1,]
+  valid_gene_types = c("lincRNA","protein_coding","IG_C_gene","IG_D_gene","IG_J_gene",
+                       "IG_V_gene", "TR_C_gene","TR_D_gene","TR_J_gene", "TR_V_gene",
+                       "3prime_overlapping_ncrna","known_ncrna", "processed_transcript",
+                       "antisense","sense_intronic","sense_overlapping")
+  valid_chromosomes = c("1","10","11","12","13","14","15","16","17","18","19",
+                        "2","20","21","22","3","4","5","6","7","8","9")
+  
+  # choose only valid gene types, valid chromosomes and TPM normalise
+  processed_se = eQTLUtils::filterSummarizedExperiment(se,
+                                                       valid_chromosomes = valid_chromosomes,
+                                                       valid_gene_types = valid_gene_types) %>% eQTLUtils::normaliseSE_tpm()
+  processed_se = processed_se[apply(SummarizedExperiment::assays(processed_se)$tpms, 1, median) > 1, ]
   
   #Perform PCA
   pca_res = eQTLUtils::transformSE_PCA(processed_se, assay_name = "tpms", n_pcs = 10, log_transform = TRUE, center = TRUE, scale. = TRUE)
-  return(pca_res$pca_matrix)
+  if (return_pca_object) {
+    return(pca_res)
+  } else {
+    return(pca_res$pca_matrix)  
+  }
 }
 
 #' Generate RNA QC (PCA, MDS, Sex) plots
