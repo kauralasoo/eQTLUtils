59d3153c7358cba77de3a1aaa521dc72e9725e1f
kerimoff <kerimov.nurlan@gmail.com>
2019-06-13 15:56:08 +0300

    add exon phenotype filtering

--- R/SummarizedExperiment_helpers.R
+++ R/SummarizedExperiment_helpers.R
@@ -87,7 +87,22 @@ makeSummarizedExperimentFromCountMatrix <- function(assay, row_data, col_data, a
   rownames(assay) <- assay$phenotype_id
   assay = assay[,shared_samples]  
   
-  message("## Checking phenotype metadata:")
+  if (quant_method == "exon_counts") {
+    # remove invalid gene types from assay
+    valid_gene_types = c("lincRNA","protein_coding","IG_C_gene","IG_D_gene","IG_J_gene",
+                         "IG_V_gene", "TR_C_gene","TR_D_gene","TR_J_gene", "TR_V_gene",
+                         "3prime_overlapping_ncrna","known_ncrna", "processed_transcript",
+                         "antisense","sense_intronic","sense_overlapping")
+    row_df = row_df[row_df$gene_type %in% valid_gene_types,]
+    
+    # remove the exons which only less than 5 samples have overlapped read(s)
+    assay = assay[rowSums(assay) > 5,] 
+    
+    shared_phenotypes <- intersect(rownames(row_df), rownames(assay))
+    assay = assay[shared_phenotypes,]  
+    row_df = row_df[shared_phenotypes,]
+  }
+  
   dummy <- assertthat::assert_that(all(rownames(assay) %in% row_df$phenotype_id), msg = "Some phenotypes in assay missing metadata information")
   
   #Make assay list
