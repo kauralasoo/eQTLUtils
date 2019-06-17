e78b9f0b45d9b8ac901697fc9174a7a9a941c605
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-06-17 23:59:00 +0300

    Build docker container for the normalisation pipeline

---
+++ builld_container.sh
@@ -0,0 +1,12 @@
+### Build the Docker container
+sudo docker build -t kauralasoo/eqtlutils .
+
+### Push to DockerHub
+docker push kauralasoo/eqtlutils
+
+### Build a local copy of the Singularity container
+singularity build coloc-wrapper.img docker://kauralasoo/eqtlutils:latest
+
+### Bind the /gpfs/hpc file system to all singularity containers (helps with testing)
+### You can add this line to your ~/.bashrc file
+export SINGULARITY_BINDPATH="/gpfs/hpc"
