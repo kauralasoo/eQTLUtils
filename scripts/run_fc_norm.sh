80c6a7aea2d346f25e7e256ee7f734b3c2186743
kerimoff <kerimov.nurlan@gmail.com>
2019-06-24 12:42:38 +0300

    delete added scripts

--- scripts/run_fc_norm.sh
+++
@@ -1,4 +0,0 @@
-Rscript normaliseCountMatrix.R\
- -c data/counts/Alasoo_test_data/Alasoo_merged_gene_counts.txt\
- -s data/sample_metadata/Alasoo_2018.tsv\
- -p data/annotations/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz
--- scripts/run_fc_qc.sh
+++
@@ -1,7 +0,0 @@
-Rscript feature_counts_qc.R\
- -c data/counts/Alasoo_test_data/Alasoo_merged_gene_counts.txt\
- -s data/sample_metadata/Alasoo_2018.tsv\
- -p data/annotations/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o ./results/RNA_QC_script_results_Alasoo\
- -m ./mbv\
- --build_html TRUE
