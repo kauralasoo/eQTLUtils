4bc4d91ee7ccdbc07788f28b810662a1e2c3e349
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-06-26 18:12:19 +0300

    Calculate arbitrary quantiles of the expression matrix.

--- R/SummarizedExperiment_QC_norm.R
+++ R/SummarizedExperiment_QC_norm.R
@@ -1,14 +1,15 @@
 #' Estimate median TPM for each gene in each sample group of a SummarizedExperiment object.
 #'
-#' Sample groups are defined accroding to the subset_by column of the colData data frame.
+#' Sample groups are defined accroding to the subset_by column of the colData data frame. The prob parmeter can be modified to estimate anu other quantile of the distribution.
 #'
 #' @param se SummarizedExperiment object
 #' @param subset_by Name of the column used for subsetting the data.
 #' @param assay_name Name of the assay used for TPM normalisation and calculation of median
+#' @param prob Specifies which quantile of the dataset to calculate. Default is prob = 0.5 (median)
 #'
 #' @return data frame of media tpm values per gene and sample group.
 #' @export
-estimateMedianTPM <- function(se, subset_by = "qtl_group", assay_name = "counts"){
+estimateMedianTPM <- function(se, subset_by = "qtl_group", assay_name = "counts", prob = 0.5){
 
   #Extract colData from SE
   col_data = SummarizedExperiment::colData(se)
@@ -27,7 +28,7 @@ estimateMedianTPM <- function(se, subset_by = "qtl_group", assay_name = "counts"
   group_se_list = purrr::map(group_list, ~subsetSEByColumnValue(tpm_se, subset_by, .))
 
   #Estimate median TPM per group
-  median_list = purrr::map(group_se_list, ~SummarizedExperiment::assays(.)[["tpms"]] %>% apply(.,1,median))
+  median_list = purrr::map(group_se_list, ~SummarizedExperiment::assays(.)[["tpms"]] %>% apply(.,1,quantile, probs = prob))
 
   #Make into a single data frame
   median_df = purrr::map_df(median_list, ~dplyr::data_frame(phenotype_id = names(.), median_tpm = .), .id = subset_by)
