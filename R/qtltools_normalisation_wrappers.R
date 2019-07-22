acd0ae2d5cf17de0148b17e31d01c93d0e784e66
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-07-22 15:35:11 +0300

    Remove expression filtering from feauteCounts normalisation

--- R/qtltools_normalisation_wrappers.R
+++ R/qtltools_normalisation_wrappers.R
@@ -25,11 +25,8 @@ qtltoolsPrepareSE <- function(se, quant_method, filter_rna_qc = TRUE, filter_gen
                                              valid_gene_types = valid_gene_types,
                                              filter_rna_qc = filter_rna_qc, filter_genotype_qc = filter_genotype_qc)
 
-    #Identify expressed genes (At least 1 count in 10% of the samples)
-    se_expressed = filterSE_expressedGenes(se_filtered, min_count = 1, min_fraction = 0.1, assay_name = "counts")
-
     #Normalise and make QTLtools matrix
-    se_norm = normaliseSE_cqn(se_expressed, assay_name = "counts")
+    se_norm = normaliseSE_cqn(se_filtered, assay_name = "counts")
 
     } else if(quant_method == "array"){
     #Filter SE to keep correct chromosomes and QC-passed samples
